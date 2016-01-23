515c588a362c7ae62fbacb308269afab30aea552
[ameba23] <[ameba23@systemli.org]>
2016-01-23 22:35:11 +0100

    added script and complete pdf

--- makezine.sh
+++ makezine.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 markdown README.md > zine.html
-htmldoc -f zine.pdf --webpage zine.html
+htmldoc -f zine.pdf --footer ..1 --webpage zine.html
+pdftk cover.pdf zine.pdf cat output fullzine.pdf
